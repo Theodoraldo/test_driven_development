7c4b11576254b1ecc3ec358526a048301cd918d2
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-03 15:11:39 +0000

    Add factorial function in test file

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,5 +3,15 @@ require './solver'
 solver = Solver.new
 
 describe Solver do
+  describe '#Returns factorial of a number' do
+    it 'first number' do
+      output = solver.factorial(4)
+      expect(output).to eq(24)
+    end
 
+    it 'second number' do
+      output = solver.factorial(10)
+      expect(output).to eq(3_628_800)
+    end
+  end
 end
