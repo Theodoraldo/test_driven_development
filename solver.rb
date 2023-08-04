5d80a6dbbee79467a5376f4d789a57d9a696d671
Theodore Asimeng-Osei <theodoraldo@gmail.com>
2023-08-04 09:22:30 +0000

    Passing at fizzbuzz test case

--- solver.rb
+++ solver.rb
@@ -18,5 +18,10 @@ class Solver
   end
 
   def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
   end
 end
