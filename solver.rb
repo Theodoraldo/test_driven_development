85be099afcffc93a9508dc6098d97b2fd9566ec6
paul-tes <pauasca8462@gmail.com>
2023-08-04 12:01:58 +0300

    Handle test case of error handling for factorial

--- solver.rb
+++ solver.rb
@@ -1,5 +1,7 @@
 class Solver
   def factorial(num)
+    raise ArgumentError, 'factorial is not defined for negative numbers' if num.negative?
+
     result = 1
     (1..num).each { |i| result *= i }
     result
