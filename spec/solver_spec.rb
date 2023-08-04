f798c0b90c8ee96d419e850a36389c9d0583e82e
paul-tes <pauasca8462@gmail.com>
2023-08-04 11:54:37 +0300

    Write test cases for factorial

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,25 @@
 require_relative 'spec_helper'
 
 RSpec.describe Solver do
+  let(:solver) { Solver.new }
+
+  describe ' => factorial -> ' do
+    context 'when the argument is 9' do
+      it 'returns 362,880' do
+        expect(solver.factorial(9)).to eq(362_880)
+      end
+    end
+
+    context 'when the argument is 0' do
+      it 'returns 1' do
+        expect(solver.factorial(0)).to eq(1)
+      end
+    end
+
+    context 'when the argument is negative' do
+      it 'raises ArgumentError' do
+        expect { solver.factorial(-4) }.to raise_error(ArgumentError)
+      end
+    end
+  end
 end
